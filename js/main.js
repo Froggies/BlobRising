6e410049a257bb74f80821f335e9490b7a047b4e
manland <rmaneschi@gmail.com>
2012-08-31 20:59:55 +0200

    move css and canvas to html instead of js code

--- js/main.js
+++ js/main.js
@@ -2,22 +2,17 @@
 
 	"use strict";
 	
-	var canvas = document.createElement("canvas");
+	var canvas = document.getElementById("canvas");
 
 	var game = new app.Game(maps, canvas);
 	// TODO remove this for contest
 	new app.editor.Editor(game);
 	
-	document.body.style.backgroundColor = 'black';
-	
 	var width = window.innerWidth;
 	var height = window.innerHeight;
 	
-	canvas.style.display = 'inline';
-	canvas.id = "canvas";
 	canvas.width = parseInt(width * 0.8);
 	canvas.height = height - 21;
-	canvas.style.border = '1px solid white';
 	document.body.appendChild(canvas);
 	
 	new app.Menu(game);
