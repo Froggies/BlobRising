88432fd64a8f703bedc71ff9477c62cbf73524a8
manland <rmaneschi@gmail.com>
2012-09-10 12:39:11 +0200

    fix bug : bad url for img wall

--- js/entities/wall.js
+++ js/entities/wall.js
@@ -13,7 +13,7 @@
 		
 		function Wall() {
 			Wall.parent.constructor.apply(this);
-			this.shape = new app.shapes.Rectangle(0,0,50,50,true,true,"#A0CD6C", null,"wall_pattern.gif");
+			this.shape = new app.shapes.Rectangle(0,0,50,50,true,true,"#A0CD6C");
 			//for debug
 			//this.radius = Math.max(this.shape.width, this.shape.height);
 		};
