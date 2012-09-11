1128eba8e507819b7ea00a05ffcc6a9b4e7e9293
Audrey <novak.audrey@gmail.com>
2012-09-11 19:49:20 +0200

    added an image for rotate machine + sprite for the blob

--- js/shapes/shape.js
+++ js/shapes/shape.js
@@ -49,43 +49,35 @@
                 var img = new Image();   
                 if (app.js.isDefined(this.urlSprite)) {
                     img.src = this.urlSprite;
-                    
+
                     var spriteY = 0;
                     
+                    var angleDegres = (physic.radian * 180) / Math.PI;
                     
-                    if (physic.angle.elements[0] > -0.25 && physic.angle.elements[0] < 0.25
-                     && physic.angle.elements[1] > 0.75) {
-                        spriteY = 88;
-                    }
-                    else if (physic.angle.elements[0] >= 0.25 && physic.angle.elements[0] < 0.75
-                     && physic.angle.elements[1] > 0.25 && physic.angle.elements[1] < 0.75) {
-                        spriteY = 110;
-                    }
-                    else if (physic.angle.elements[0] > 0.75
-                     && physic.angle.elements[1] > 0.75) {
+                    if(angleDegres > 0 && angleDegres <= 45) {
                         spriteY = 132;
                     }
-                    else if (physic.angle.elements[0] >= 0.25 && physic.angle.elements[0] < 0.75
-                     && physic.angle.elements[1] > -0.25 && physic.angle.elements[1] < -0.75) {
+                    else if(angleDegres > 45 && angleDegres <= 90) {
                         spriteY = 154;
                     }
-                    else if (physic.angle.elements[0] > -0.25 && physic.angle.elements[0] < 0.25
-                     && physic.angle.elements[1] > -0.75) {
+                    else if(angleDegres > 90 && angleDegres <= 135) {
                         spriteY = 0;
                     }
-                    else if (physic.angle.elements[0] > -0.25 && physic.angle.elements[0] < -0.75
-                     && physic.angle.elements[1] > -0.25 && physic.angle.elements[1] < -0.75) {
+                    else if(angleDegres > 135 && angleDegres <= 180) {
                         spriteY = 22;
                     }
-                    else if (physic.angle.elements[0] > -0.75
-                     && physic.angle.elements[1] > -0.25 && physic.angle.elements[1] < 0.25) {
+                    else if(angleDegres > 180 && angleDegres <= 225) {
                         spriteY = 44;
                     }
-                    else if (physic.angle.elements[0] > -0.25 && physic.angle.elements[0] < -0.75
-                     && physic.angle.elements[1] > 0.25 && physic.angle.elements[1] < 0.75) {
+                    else if(angleDegres > 225 && angleDegres <= 270) {
                         spriteY = 66;
                     }
-                    console.log("angle : "+physic.angle.elements[0]+" y "+physic.angle.elements[1]);
+                    else if(angleDegres > 270 && angleDegres <= 315) {
+                        spriteY = 88;
+                    }
+                    else if(angleDegres > 315 && angleDegres <= 360) {
+                        spriteY = 110;
+                    }
                     
                     context.drawImage(img,0, spriteY, 20, 20, this.x, this.y, this.width, this.height);
                 }
