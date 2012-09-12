e821d98b485345e0ba98e6ac5477948b74b568b0
manland <rmaneschi@gmail.com>
2012-09-12 13:41:54 +0200

    hierarchical architecture between magnet and well (save lignes)

--- js/entities/magnet.js
+++ js/entities/magnet.js
@@ -3,52 +3,18 @@
 	"use strict";
 
     var 
-		Entity = app.entities.Entity,
-		inherit = app.js.inherit,
-		isDefined = app.js.isDefined;
+		Well = app.entities.Well,
+		inherit = app.js.inherit;
 
 	app.entities.Magnet = function() {
 
-		inherit(Magnet, Entity);
+		inherit(Magnet, Well);
 		
 		function Magnet() {
 			Magnet.parent.constructor.apply(this);
-			this.nbBlobMax = 5;
-			this.nbBlob = 0;
-			this.last = false;
 			this.shape = new app.shapes.Rectangle(0,0,50,52,"#FFCC00","img/magnet.gif");
-		    this.attraction = new app.shapes.Ellipse();
-		    this.attraction.color = "rgba(1,2,3,1)";
-		    this.attracted = 0;
+			this.factorAttraction = 3;
 		};
-		
-		Magnet.prototype.update = function(translation, map) {
-		    if(this.nbBlobMax <= this.nbBlob && this.last !== true) {
-		        this.dead(map);
-		    } else {
-		        Magnet.parent.update.call(this, translation, map);
-		    }
-		}
-
-		Magnet.prototype.draw = function(context, map) {
-		    if(map.showNoneEntities) {
-		        this.attraction.x = this.shape.x + (this.shape.width / 2);
-		        this.attraction.y = this.shape.y + (this.shape.height / 2);
-		        this.attraction.width = this.shape.width * 3;
-		        this.attraction.height = this.attraction.width;
-
-                context.save();                
-		        context.shadowColor = this.shape.color;
-                context.shadowOffsetX = 0;
-                context.shadowOffsetY = 0;
-                context.shadowBlur = 6;
-		        
-		        this.attraction.draw(context);
-		        context.restore();
-	        }
-
-		    Magnet.parent.draw.call(this, context, map);
-		}
 		
 		return Magnet;
 
--- js/entities/well.js
+++ js/entities/well.js
@@ -20,6 +20,7 @@
 		    this.attraction = new app.shapes.Ellipse();
 		    this.attraction.color = "rgba(1,2,3,1)";
 		    this.attracted = 0;
+		    this.factorAttraction = 2;
 		};
 		
 		Well.prototype.update = function(translation, map) {
@@ -34,8 +35,8 @@
 		    if(map.showNoneEntities) {
 		        this.attraction.x = this.shape.x + (this.shape.width / 2);
 		        this.attraction.y = this.shape.y + (this.shape.height / 2);
-		        this.attraction.width = this.shape.width * 2;
-		        this.attraction.height = this.shape.width * 2;
+		        this.attraction.width = this.shape.width * this.factorAttraction;
+		        this.attraction.height = this.attraction.width;
 
                 context.save();                
 		        context.shadowColor = this.shape.color;
